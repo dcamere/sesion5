992cf5249b90682020eee4cbde1eaebbe354303d
miguel <miguel@wyracocha.com>
2021-06-27 15:02:10 -0500

    fix sintax

--- index.js
+++ index.js
@@ -5,12 +5,10 @@ App.get('/', (req, res) => {
     res.send('ok');
 })
 
-App.listen(`0.0.0.0:${process.env.PORT}`, (e) => {
+App.listen(process.env.PORT, (e) => {
     if (e) {
         console.log(e);
         process.exit(1);
     }
     console.log('app running');
 });
-
-//
